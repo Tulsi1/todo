0e6a39589a1f347f3a8a7d93fb766662c928b969
Tulsi1 <56266852+Tulsi1@users.noreply.github.com>
2020-01-06 04:00:56 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,100 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Remove");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Remove");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+ 
+
+   var spa = document.createElement("SPAN");
+   var tx = document.createTextNode("Edit");
+   spa.className = "edit";
+   spa.appendChild(tx);
+   li.appendChild(spa);
+
+   for (j = 0; j < edit.length; j++) {
+     edit[j].onclick = function() {
+        var divi = this.parentElement;
+       divi.style.display = "checked";
+     }
+   }
+
+
+//   //Create an Edit button
+ var myNode = document.getElementsByTagName("LI");
+ var j;
+ for (j = 0; j < myNode.length; j++) {
+   var spa = document.createElement("SPAN");
+   var tx = document.createTextNode("Edit");
+   spa.className = "edit";
+   spa.appendChild(tx);
+   myNode[j].appendChild(spa); 
+}
+    
+// click on edit button to change
+// function change(elmnt){
+//     var x = input.value;
+//     if (x != ""){
+//         elmnt.childNodes[1].childNodes[0].nodeValue = x;
+//     }
+//     var sub = document.getElementById("addBt");
+//     input.value = "";
+//     sub.onclick = function() {addtodo()};
+//     var text = elmnt.childNodes[1];
+//     text.className = "todo";
+//     text.onclick = function() {changeclass(elmnt)};
+//     var chan = elmnt.childNodes[0];
+//     chan.childNodes[0].src = "css/img/circle-li.png";
+// }
+   }
+ 
